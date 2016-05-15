e999861bec545a83becced25f0ae68369b3a9583
Amir <amir_rajabifar@yahoo.com.au>
2016-05-16 00:38:59 +0800

    Added the movement algorithm

--- Car_Manager/Car_Manager.ino
+++ Car_Manager/Car_Manager.ino
@@ -11,6 +11,19 @@
 #define esc_pin 6
 #define servo_pin 5
 
+#define low_mid 1400
+#define high_mid 1600
+
+#define dead_zone 1700
+
+#define esc_high 180
+#define esc_mid 90
+#define esc_low 0
+
+#define servo_high 180
+#define servo_mid 90
+#define servo_low 0
+
 byte last_channel_1, last_channel_2, last_channel_3, last_channel_4, last_channel_5, last_channel_6;
 int receiver_input_channel_1, receiver_input_channel_2, receiver_input_channel_3, receiver_input_channel_4, receiver_input_channel_5, receiver_input_channel_6;
 int channel_1, channel_2, channel_3, channel_4, channel_5, channel_6;
@@ -18,6 +31,8 @@ int esc, servo;
 unsigned long timer_channel_1, timer_channel_2, timer_channel_3, timer_channel_4, timer_channel_5, timer_channel_6;
 unsigned long timer_1, timer_2, timer_3, timer_4, timer_5, timer_6, current_time;
 
+Servo esc;
+Servo serco;
  
 void setup ()
 {
@@ -34,18 +49,65 @@ void setup ()
   PCMSK2 |= (1 << PCINT20); //Set PCINT20 (Analog input A12) to trigger an interrupt on state change.
   PCMSK2 |= (1 << PCINT21); //Set PCINT21 (Analog input A13) to trigger an interrupt on state change.
 
+  //pin connection to servo objects
+  servo.attach(servo_pin);
+  esc.attach(esc_pin);
+
   Serial.begin(115200);
 }
 
 void loop ()
 {
+  //map the values
   channel_1 = map(receiver_input_channel_1, fromLow, fromHigh, toLow, toHigh);
   channel_2 = map(receiver_input_channel_2, fromLow, fromHigh, toLow, toHigh);
   channel_3 = map(receiver_input_channel_3, fromLow, fromHigh, toLow, toHigh);
   channel_4 = map(receiver_input_channel_4, fromLow, fromHigh, toLow, toHigh);
   channel_5 = map(receiver_input_channel_5, fromLow, fromHigh, toLow, toHigh);
   channel_6 = map(receiver_input_channel_6, fromLow, fromHigh, toLow, toHigh);
-
+  //set low point
+  if (channel_1 < toLow) channel_1 = toLow;
+  if (channel_2 < toLow) channel_2 = toLow;
+  if (channel_3 < toLow) channel_3 = toLow;
+  if (channel_4 < toLow) channel_4 = toLow;
+  if (channel_5 < toLow) channel_5 = toLow;
+  if (channel_6 < toLow) channel_6 = toLow;
+  //set high point
+  if (channel_1 > toHigh) channel_1 = toHigh;
+  if (channel_2 > toHigh) channel_2 = toHigh;
+  if (channel_3 > toHigh) channel_3 = toHigh;
+  if (channel_4 > toHigh) channel_4 = toHigh;
+  if (channel_5 > toHigh) channel_5 = toHigh;
+  if (channel_6 > toHigh) channel_6 = toHigh;
+  //if the trasmitter is off turn esc off and ceter the servo
+  if(channel_1 <= toLow && channel_2 <= toLow && channel_3 <= toLow && channel_4 <= toLow && channel_5 <= toLow && channel_6 <= toLow)
+  {
+      esc.write(esc_mid);
+      servo.write(servo_mid);
+  }
+  //else do the noraml routine
+  else 
+  {
+    //write the throttle value to esc 
+    if(channel_3 > high_mid && channel_5 < dead_zone) //forward
+    {
+      esc.write(map(channel_3, toLow, toLow, esc_mid, esc_high));     
+    }
+    else if(channel_3 < low_mid && channel_5 > dead_zone) //reverse
+    {
+      esc.write(map(channel_3, toLow, toHigh, esc_mid, esc_low));
+    }  
+    //write the steering value to servo
+    if(channel_1 > high_mid) // left
+    {
+      servo.write(map(channel_1, toLow, toHigh, servo_mid, servo_high));
+    }
+    else if(channel_1 < low_mid) //right
+    {
+      servo.write(map(channel_1, toLow, toHigh, servo_mid, servo_low));
+    }
+  }
+   
 // debug
   Serial.print("Channel 1 = ");
   Serial.print(channel_1);
@@ -69,63 +131,67 @@ void loop ()
 }
 
 
+
+
+
+//This routine is called every time input A8, A9, A10, A11, A12 or A13 changed state
 ISR(PCINT2_vect)
 {
   current_time = micros();
   //Channel 1=========================================
   if(PINK & B00000001)
-  {                                        //Is input 8 high?
+  {                                                            //Is input A8 high?
   if(last_channel_1 == 0)
-  {                                   //Input 8 changed from 0 to 1
-    last_channel_1 = 1;                                      //Remember current input state
-    timer_1 = current_time;                                  //Set timer_1 to current_time
+  {                                                            //Input A8 changed from 0 to 1
+    last_channel_1 = 1;                                        //Remember current input state
+    timer_1 = current_time;                                    //Set timer_1 to current_time
   }
   }
   else if(last_channel_1 == 1)
-  {                                //Input 8 is not high and changed from 1 to 0
+  {                                                            //Input A8 is not high and changed from 1 to 0
     last_channel_1 = 0;                                        //Remember current input state
     receiver_input_channel_1 = current_time - timer_1;         //Channel 1 is current_time - timer_1
   }
   //Channel 2=========================================
   if(PINK & B00000010 )
-  {                                       //Is input 9 high?
+  {                                                            //Is input A9 high?
     if(last_channel_2 == 0)
-    {                                   //Input 9 changed from 0 to 1
+    {                                                          //Input A9 changed from 0 to 1
       last_channel_2 = 1;                                      //Remember current input state
       timer_2 = current_time;                                  //Set timer_2 to current_time
     }
   }
   else if(last_channel_2 == 1)
-  {                                //Input 9 is not high and changed from 1 to 0
+  {                                                            //Input A9 is not high and changed from 1 to 0
     last_channel_2 = 0;                                        //Remember current input state
     receiver_input_channel_2 = current_time - timer_2;         //Channel 2 is current_time - timer_2
   }
   //Channel 3=========================================
   if(PINK & B00000100 )
-  {                                       //Is input 10 high?
+  {                                                            //Is input A10 high?
     if(last_channel_3 == 0)
-    {                                   //Input 10 changed from 0 to 1
+    {                                                          //Input A10 changed from 0 to 1
       last_channel_3 = 1;                                      //Remember current input state
       timer_3 = current_time;                                  //Set timer_3 to current_time
     }
   }
   else if(last_channel_3 == 1)
-  {                                //Input 10 is not high and changed from 1 to 0
+  {                                                            //Input A10 is not high and changed from 1 to 0
     last_channel_3 = 0;                                        //Remember current input state
     receiver_input_channel_3 = current_time - timer_3;         //Channel 3 is current_time - timer_3
 
   }
   //Channel 4=========================================
   if(PINK & B00001000 )
-  {                                       //Is input 11 high?
+  {                                                            //Is input A11 high?
     if(last_channel_4 == 0)
-    {                                   //Input 11 changed from 0 to 1
+    {                                                          //Input A11 changed from 0 to 1
       last_channel_4 = 1;                                      //Remember current input state
       timer_4 = current_time;                                  //Set timer_4 to current_time
     }
   }
   else if(last_channel_4 == 1)
-  {                                //Input 11 is not high and changed from 1 to 0
+  {                                                            //Input A11 is not high and changed from 1 to 0
     last_channel_4 = 0;                                        //Remember current input state
     receiver_input_channel_4 = current_time - timer_4;         //Channel 4 is current_time - timer_4
   }
